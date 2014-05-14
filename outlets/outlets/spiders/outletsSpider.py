5f923f83c4e74bd3351a7cf4e84ba13214d2cb54
snoopyxdy <snoopyxdy@gmail.com>
2014-05-14 22:22:58 +0800

    one ok

--- outlets/outlets/spiders/outletsSpider.py
+++ outlets/outlets/spiders/outletsSpider.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python 
 # -*- coding: utf-8 -*-
 import scrapy
+import os
+import urlparse
 from scrapy.spider import BaseSpider
 from pyquery import PyQuery as pq
 import logging
+import json
 
 #设置logger模块
 logger = logging.getLogger('mylogger')
@@ -14,7 +17,10 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
+
+
 class OutletsSpider(BaseSpider):
+    curPath = os.path.split(os.path.realpath(__file__))[0]
     name = "outlets"
     allowed_domains = ["premiumoutlets.com"]
     scrapyHost = 'http://www.premiumoutlets.com/'
@@ -46,7 +52,7 @@ class OutletsSpider(BaseSpider):
         #获得url之后,将分别对这些url进行过滤和继续抓取,请求地址获取每个outlets店铺的内容
         return [self.urls[0]]
     
-    #定义解析outlet店铺首页的方法
+    #定义解析outlet店铺首页的方法，ex：http://www.premiumoutlets.com/outlets/outlet.asp?id=5
     def parseShopHome(self, response):    
         if response.status != 200 :
             logger.error('{0} error code: {1}'.format(response.url, str(response.status)))
@@ -58,7 +64,7 @@ class OutletsSpider(BaseSpider):
         return [scrapy.Request(url, callback=self.parseShop)]
     
     
-    #定义解析outlet店铺商品页面的方法
+    #定义解析outlet店铺商品页面的方法，ex：http://www.premiumoutlets.com/outlets/store_listing.asp?id=5
     def parseShop(self, response):
         if response.status != 200 :
             logger.error('{0} error code: {1}'.format(response.url, str(response.status)))
@@ -76,31 +82,130 @@ class OutletsSpider(BaseSpider):
         tel = '(' + tempAry[1]
         
         mapUrl = pqBody('.CenterMap').eq(0).attr('onclick')
-        mapUrl = mapUrl.split('(\'')[1].split('\')')[0]
+        mapUrl = self.scrapyHost+'outlets/' + mapUrl.split('(\'')[1].split('\')')[0]
+        
+        pdfUrl = pqBody('#Padder').find('a').eq(2).attr('href') 
+        pdfUrl = urlparse.urljoin(self.scrapyHost+'outlets/', pdfUrl)
         
         print '###############'
-        print shopName
-        print address
-        print tel
-        print mapUrl
-        
-        print '###############'
-        
-      
-        
-        
+        #print shopName
+        #print address
+        #print tel
+        #print mapUrl
+        # print pdfUrl
+        # print '###############'
+               
         outletDict = {
             'name': shopName,
-            'address' : 1,
-            'tel':1,
+            'address' : address,
+            'tel':tel,
             'category':1,
-            'shops':[],
-            'pdf':1,
-            'img':1
+            'pdfUrl':pdfUrl,
         }
         
-        pass        
+        return [scrapy.Request(mapUrl, callback=self.parseMap, meta={'outletDict':outletDict})]        
         
         
+    #解析地图页面，ex：http://www.premiumoutlets.com/outlets/store_listing_map.asp?id=5
+    def parseMap(self, response):
+        if response.status != 200 :
+          logger.error('{0} error code: {1}'.format(response.url, str(response.status)))
+          return
+        
+        #获得店铺id
+        parsed = urlparse.urlparse(response.url)
+        shopId = str(urlparse.parse_qs(parsed.query)['id'][0]) or 'nan'
+        
+        outletDict = response.meta["outletDict"]
+        pqBody = pq(response.body)
+        shops = []
+        imgs = []
+        
+        #定义解析table的tr
+        def parseTr(i,tr):
+            pqtr = pq(tr)
+            pqtd = pqtr.find('td')
+            if i == 0:
+                pass
+            elif i == 1:
+                pass
+            else:
+                #保存店铺类型，店铺编号，店铺名称，店铺电话
+                parentTable = pqtr.parent()
+                category = parentTable.find('tr').eq(0).find('td').eq(0).text().encode('utf-8').strip()
+                suit = pqtd.eq(0).text().encode('utf-8').strip()
+                store = pqtd.eq(0).text().encode('utf-8').strip()
+                telephone = pqtd.eq(0).text().encode('utf-8').strip()
+                
+                shops.append({
+                    'category':category,
+                    'suit':suit,
+                    'store':store,
+                    'telephone':telephone
+                })
+            
+        
+        #循环遍历table
+        def parseTable(i,table):
+            pqTable = pq(table)
+            pqTable.find('tr').each(parseTr)
+        
+        def parseImg(i, img):
+            pqImg = pq(img)
+            imgUrl = self.scrapyHost+'outlets/' + pqImg.attr('src')
+            imgs.append(
+                scrapy.Request(imgUrl, callback=self.parseImg, meta={'shopId':shopId, 'num':i})
+            )
+            
+        #获取内容
+        shopTableBox = pqBody('.BorderSet')
+        #循环获取店铺内容
+        shopTableBox.each(parseTable)
+        #循环获取图片内容
+        pqBody('.screenImg').each(parseImg)
+        
+        print '$$$$$$$$$$$$$'
+        #print imgs
+        #print shops
+        print outletDict
+        print shopId
+        print outletDict['pdfUrl']
+                
+        filename = self.curPath + os.sep + 'json' + os.sep + 'json_' + shopId + '.json'
+        with open(filename, 'wb') as f:
+            f.write(json.dumps(shops))
+          
+        imgs.append(
+            scrapy.Request(outletDict['pdfUrl'], callback=self.parsePdf, meta={'shopId':shopId})
+            )
+        
+        #去下载图片和pdf
+        return imgs
+        
+        
+    #处理图片保存图片
+    def parseImg(self,response):
+        if response.status != 200 :
+          logger.error('{0} error code: {1}'.format(response.url, str(response.status)))
+          return
+        shopId = response.meta['shopId']
+        
+        filename = self.curPath + os.sep + 'img' + os.sep + 'map_' + shopId + '.gif'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        pass
+    
+    #处理pdf文件保存文件
+    def parsePdf(self,response):
+        if response.status != 200 :
+          logger.error('{0} error code: {1}'.format(response.url, str(response.status)))
+          return
+        shopId = response.meta['shopId']
+        
+        filename = self.curPath + os.sep + 'pdf' + os.sep + 'pdf_' + shopId + '.pdf'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        pass
+    
     def toDb(self, itemDict):
         pass
--- outlets/start.py
+++ outlets/start.py
@@ -1,3 +1,45 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+
 from scrapy.cmdline import execute
+import os
+
+def mkdir(path):
+    # 引入模块
+    
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+ 
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists=os.path.exists(path)
+ 
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        print path+' 创建成功'
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print path+' 目录已存在'
+        return False
+
+
+#计算路径，然后创建文件夹
+curPath = os.path.split(os.path.realpath(__file__))[0]
+targetPath = curPath + os.sep + 'outlets' + os.sep + 'spiders' + os.sep
+
+pdfDir = targetPath + 'pdf'
+imgDir = targetPath + 'img'
+jsonDir = targetPath + 'json'
+
+mkdir(pdfDir)
+mkdir(imgDir)
+mkdir(jsonDir)
 
 execute()
